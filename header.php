fdf441e6594597d9c47a3a2f94505e0fdf4ec987
ncir jaweher <jaweherncir@gmail.com>
2023-03-17 23:08:39 +0100

    php pages

--- header.php
+++ header.php
@@ -154,110 +154,15 @@
     <!-- /.sidebar -->
   </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0">Accueil</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Accueil</a></li>
-              <li class="breadcrumb-item active">Gestion De Stock</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
-        <div class="row">
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-info">
-              <div class="inner">
-                <h3>150</h3>
-
-                <p>Total de Produits</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-bag"></i>
-              </div>
-              <a href="#" class="small-box-footer"> <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-success">
-              <div class="inner">
-                <h3>53<sup style="font-size: 20px"></sup></h3>
-
-                <p>Fournisseurs</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person"></i>
-              </div>
-              <a href="#" class="small-box-footer"><i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
-
-                <p>Clients</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#" class="small-box-footer"> <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-danger">
-              <div class="inner">
-                <h3>65</h3>
-
-                <p>Factures</p>
-              </div>
-              <div class="icon">
-                <i class="fa fa-file-text-o"></i>
-              </div>
-              <a href="#" class="small-box-footer"> <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
-          <!-- ./col -->
-        </div>
-        
-      </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
 
 
 
-
-  </div>
-
-
-
-  <!-- /.content-wrapper -->
-  <?php  require_once "footer.php" ;?>
- 
+    
 
   <!-- Control Sidebar -->
-  <?php  require_once "header.php" ;?>
+  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control sidebar content goes here -->
+  </aside>
   <!-- /.control-sidebar -->
 </div>
 <!-- ./wrapper -->
--- newFornisseur.php
+++ newFornisseur.php
@@ -54,10 +54,10 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index.html" class="nav-link">Home</a>
+        <a href="index.php" class="nav-link">Home</a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="/compte.html" class="nav-link">Espace Admin</a>
+        <a href="/compte.php" class="nav-link">Espace Admin</a>
       </li>
     </ul>
 
@@ -144,7 +144,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index.html" class="brand-link">
+    <a href="index.php" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Administrateur</span>
     </a>
@@ -168,7 +168,7 @@
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
         
           <li class="nav-item">
-            <a href="newStock.html" class="nav-link">
+            <a href="newStock.php" class="nav-link">
               <i class="nav-icon fas fa-th"></i>
               <p> 
                 Stock
@@ -178,7 +178,7 @@
           </li>
 
           <li class="nav-item">
-            <a href="newClient.html" class="nav-link">
+            <a href="newClient.php" class="nav-link">
               <i class="nav-icon fas fa-user"></i>
               <p>
                Client
@@ -188,7 +188,7 @@
        
           </li>
           <li class="nav-item">
-            <a href="newFornisseur.html" class="nav-link">
+            <a href="newFornisseur.php" class="nav-link">
               <i class="nav-icon fas fa-users"></i>
               <p>
                Fournisseur
@@ -198,7 +198,7 @@
        
           </li>
           <li class="nav-item">
-            <a href="newCommande.html" class="nav-link">
+            <a href="newCommande.php" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
                Commande
@@ -208,7 +208,7 @@
        
           </li>
           <li class="nav-item">
-            <a href="facture.html" class="nav-link">
+            <a href="facture.php" class="nav-link">
               <i class="nav-icon fas fa-file"></i>
               <p>Facture</p>
             </a>
